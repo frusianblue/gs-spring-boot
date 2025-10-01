b36bd0002d7de4de5b689ce058b1fbbd229c5523
프루시안블루 <frusianblue@gmail.com>
2025-10-02 03:46:45 +0900

    풀리퀘스트 테스트3

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
-`   // PR 테스트
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
