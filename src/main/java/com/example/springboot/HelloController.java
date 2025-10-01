ad0e41d48e9ae950e0c54a24a54878c47700897b
프루시안블루 <frusianblue@gmail.com>
2025-10-02 03:29:35 +0900

    풀리퀘스트 테스트2

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    // 풀리퀘스트2
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
