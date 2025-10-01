0b83559395c531f1798a0470f3523551df1829fe
프루시안블루 <frusianblue@gmail.com>
2025-10-02 03:53:18 +0900

    PR4

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    // 풀리퀘스트2
+    // PR2
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
